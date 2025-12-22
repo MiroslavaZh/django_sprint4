e5062c3062c7cc4dbe6dd81f50f10fe46440f1be
MiroslavaZh <91941416+MiroslavaZh@users.noreply.github.com>
2025-12-22 20:59:41 +0300

    Update urls.py

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,38 +1,34 @@
 from django.urls import include, path
-
 from . import views
 
 app_name = 'blog'
 
+# URL для постов
 post_urls = [
-    path('create/',
-         views.create_post, name='create_post'),
-    path('<int:post_id>/',
-         views.post_detail, name='post_detail'),
-    path('<int:post_id>/edit/',
-         views.edit_post, name='edit_post'),
-    path('<int:post_id>/delete/',
-         views.delete_post, name='delete_post'),
-    path('<int:post_id>/comment/',
-         views.add_comment, name='add_comment'),
-    path('<int:post_id>/edit_comment/<int:comment_id>/',
-         views.edit_comment, name='edit_comment'),
-    path('<int:post_id>/delete_comment/<int:comment_id>/',
-         views.delete_comment, name='delete_comment'),
+    path('create/', views.create_post, name='create_post'),
+    path('post/<int:post_pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:post_pk>/edit/', views.edit_post, name='edit_post'),
+    path('post/<int:post_pk>/delete/', views.delete_post, name='delete_post'),
+    path('post/<int:post_pk>/comment/add/', views.add_comment, name='add_comment'),
+    path('post/<int:post_pk>/comment/<int:comment_pk>/edit/', views.edit_comment, name='edit_comment'),
+    path('post/<int:post_pk>/comment/<int:comment_pk>/delete/', views.delete_comment, name='delete_comment'),
 ]
 
+# URL для категорий
+category_urls = [
+    path('category/<str:category_name>/', views.category_posts, name='category_posts'),
+]
+
+# URL для профиля пользователя
 profile_urls = [
-    path('edit/',
-         views.edit_profile, name='edit_profile'),
-    path('<slug:username>/',
-         views.profile, name='profile'),
+    path('edit/', views.edit_profile, name='edit_profile'),
+    path('user/<str:username>/', views.profile, name='profile'),  # теперь str вместо slug
 ]
 
+# Главные URL проекта
 urlpatterns = [
-    path('',
-         views.index, name='index'),
-    path('category/<slug:category_slug>/',
-         views.category_posts, name='category_posts'),
+    path('', views.index, name='index'),
     path('posts/', include(post_urls)),
+    path('categories/', include(category_urls)),
     path('profile/', include(profile_urls)),
 ]
