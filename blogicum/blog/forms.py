0e2986791715713a3821f1a874306be7b10e8cea
MiroslavaZh <91941416+MiroslavaZh@users.noreply.github.com>
2025-12-22 20:57:37 +0300

    Update forms.py

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -1,24 +1,21 @@
 from django import forms
-
+from django.forms.widgets import DateTimeInput
 from .models import Post, Comment, User
 
-
 class PostForm(forms.ModelForm):
-
     class Meta:
         model = Post
-        exclude = ('author',)
-
+        fields = ('title', 'content', 'is_published', 'pub_date')
+        widgets = {
+            'pub_date': DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
 
 class CommentForm(forms.ModelForm):
-
     class Meta:
         model = Comment
         fields = ('text',)
 
-
 class UserForm(forms.ModelForm):
-
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
